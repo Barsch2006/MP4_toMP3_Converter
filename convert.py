20b64dfdf24f7be1407efa663a58619dc509a732
Barsch2006 <cchr98756@gmail.com>
2022-12-15 16:25:16 +0100

    changes

--- convert.py
+++ convert.py
@@ -1,15 +1,45 @@
 import string
 import os
 from moviepy.editor import *
+from pytube import YouTube
 
-""" 
-mp4dir: string == the relative Path to the mp4-file
-outputdir: string == the relatie path to the output dir
-mp3dir: string == the name for the output-file. Should end with '.mp3'
-"""
+
 def convert(mp4dir: string, mp3name: string, outputdir: string) -> None:
     video = VideoFileClip(os.path.join(mp4dir))
     video.audio.write_audiofile(os.path.join(outputdir,mp3name))
     video.close()
+    if os.path.exists(mp4dir):
+        os.remove(mp4dir)
 
-convert("example_video.mp4","output_name.mp3")
+
+def download(yt):
+    yt.download("G:\YTDownloader\output\mp4", yt.title)
+    return "G:\YTDownloader\output\mp4/" + yt.title
+
+def getURLs():
+    f = open("G:\YTDownloader/try.txt", 'r')
+    lines = f.readlines()
+    urls = []
+    for line in lines:
+        urls.append(line[0:-1])
+    num = 1
+    for url in urls:
+        try:
+            yt = YouTube(url)
+            yt = yt.streams.get_highest_resolution()
+            mp4dir = download(yt)
+            mp3name = yt.title + ".mp3"
+            convert(mp4dir, mp3name, "G:\YTDownloader\output\sec_try")
+            num = num + 1
+        except:
+            print("Error nicht möglich #" + str(num))
+            num = num + 1
+            continue
+
+def readDir(dir):
+    i=0
+    for file in os.listdir("G:\YTDownloader\output\sec_try"):
+        if file.endswith('.mp3'):
+            print(file)
+            i+=1
+    print('the total number of files: ' +str(i))
